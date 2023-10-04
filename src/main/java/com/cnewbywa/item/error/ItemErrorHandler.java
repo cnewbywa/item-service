e9afd021497209ed51da04b9698d89f3c6ceb4c1
cnewbywa <cnewbywa@gmail.com>
2023-10-04 16:08:51 +0300

    Add logging to error handler

--- src/main/java/com/cnewbywa/item/error/ItemErrorHandler.java
+++ src/main/java/com/cnewbywa/item/error/ItemErrorHandler.java
@@ -4,21 +4,30 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @ControllerAdvice
+@Slf4j
 public class ItemErrorHandler {
 
 	@ExceptionHandler(ItemNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-	public void handleItemNotFoundException() {}
+	public void handleItemNotFoundException(RuntimeException re) {
+		log.error(re.getMessage(), re);
+	}
 	
 	@ExceptionHandler(UsernameNotFoundException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-	public void handleUserNotFoundException() {}
+	public void handleUserNotFoundException(RuntimeException re) {
+		log.error(re.getMessage(), re);
+	}
 	
 	@ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public void handleException() {}
+	public void handleException(RuntimeException re) {
+		log.error(re.getMessage(), re);
+	}
 }
