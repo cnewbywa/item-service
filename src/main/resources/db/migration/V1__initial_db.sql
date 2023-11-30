5aca6bec4603338df67e2b951efdfc58fd68026c
cnewbywa <cnewbywa@gmail.com>
2023-11-30 09:44:16 +0200

    Added support for Flyway migrations

---
+++ src/main/resources/db/migration/V1__initial_db.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS item;
+
+CREATE TABLE IF NOT EXISTS item.item (
+    id serial PRIMARY KEY,
+    item_id VARCHAR(40),
+    name VARCHAR(50),
+    description VARCHAR(500),
+    create_time TIMESTAMPTZ,
+    update_time TIMESTAMPTZ,
+    created_by VARCHAR(75),
+    modified_by VARCHAR(75),
+    version serial);
